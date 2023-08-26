33dee2ae98a1a1c9d003d2580fb3cb33124a7005
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-08-26 16:09:04 -0300

    installing the library faker and creating a folder called utils with a file called create_contacts.py to generate 1000 random data to populate the model Contact and Category

--- project/contact/models.py
+++ project/contact/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Category(models.Model):
+
+    # adjusting the plural name of Category. Django by default will add a 'S' to evey Model. As a rule of thumb this works but other names the result would be a little strange (Categorys). So I am telling django the plural of the word
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -17,6 +24,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
