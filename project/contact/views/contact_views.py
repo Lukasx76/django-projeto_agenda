bef6b3d9b5e2bfe4cf7c9ad874659ecabf0bd051
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-08-30 12:12:46 -0300

    creating a pagination and a view, template and url for creating an user

--- project/contact/views/contact_views.py
+++ project/contact/views/contact_views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
+
 def index(request):
 
-    contacts = Contact.objects.filter(show=True).order_by('-id')[:20]
-    context = {'contacts': contacts,
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+    paginator = Paginator(contacts, 10) 
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {'page_obj': page_obj,
                'site_title': 'Contatos - '}
+    
 
     return render(
         request,
@@ -42,8 +50,13 @@ def search(request):
         Q(phone__icontains=search_value) |
         Q(email__icontains=search_value)
         )\
-    .order_by('-id')
-    context = {'contacts': contacts,
+    .order_by('-id')    
+
+    paginator = Paginator(contacts, 10) 
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {'page_obj': page_obj,
                'site_title': 'Contatos - '}
 
     return render(
