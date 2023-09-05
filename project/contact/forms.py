9ec37052e3c8acfbae0dbd6eaf4e974270130458
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-09-05 14:43:19 -0300

    adding more fields to user register and messages when a user is created

--- project/contact/forms.py
+++ project/contact/forms.py
@@ -2,10 +2,8 @@ from contact.models import Contact
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
-
-class RegisterForm(UserCreationForm):
-    ...
-
+from django.contrib.auth.models import User
+    
 class ContactForm(forms.ModelForm):
 
     first_name = forms.CharField(
@@ -48,3 +46,30 @@ class ContactForm(forms.ModelForm):
             self.add_error('last_name', error_msg)
             
         return super().clean()
+    
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+        )
+    last_name = forms.CharField(
+        required=True, 
+        min_length=3,
+        )
+    email = forms.EmailField()
+
+    class Meta():
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+            )
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Email já existe', code='invalid')
+                
+            )
